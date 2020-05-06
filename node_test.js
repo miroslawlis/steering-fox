2e586aa3cc3bc4019e33c9d7519555e299cf62c2
Mirosław Lis <miroslawlis@users.noreply.github.com>
2020-05-06 16:52:33 +0200

    a

--- node_test.js
+++ node_test.js
@@ -1,7 +1,8 @@
 var buff2 = new Buffer.from([128, 10, 255, 36, 3, 0, 43, 49, 50, 46, 53, 97, 128, 15, 255, 36, 2, 0, 49, 48, 46, 49, 49, 46, 50, 48, 49, 57, 93, 128, 5, 191, 24, 0, 6, 36]);
 var comp_buff = new Buffer.from([164, 0, 1]);
-var buff3 = new Buffer.from([0x40, 0x01, 19, 40]);
-var buff4 = new Buffer.from([36,1,0,49,55,58,48,57,32,32]);
+var buff3 = new Buffer.from([0x40, 0x01, '18', '22'])
+var buff4 = new Buffer.from([0x40, 0x01, 18, 22]);
+
 
 console.log('1. #:' + buff2.toString('ascii') + ':#');
 
@@ -10,7 +11,7 @@ console.log(x);
 
 console.log("buff3: ", buff3.toString('hex'));
 
-console.log('buff4 ', buff4.toString('hex'));
-console.log('buff4 ' + buff4.slice(3,9));
+console.log('buff4: ', buff4.toString('hex'));
+console.log('buff4: ' + buff4.slice(3,9));
 
 
