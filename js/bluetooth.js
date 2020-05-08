8f6448f466ad1a65d8a7d85f32ebd78010243360
Mirosław Lis <miroslawlis@users.noreply.github.com>
2020-05-08 22:04:56 +0200

    fixes

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -9,57 +9,62 @@ window.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('bluetooth').addEventListener("click", function () {
         // bluetoothctl init
-        bt.Bluetooth();
+        try {
+            bt.Bluetooth();
 
-        let btConnectedDevicesDOM = document.querySelector('.settings .bluetooth .pairedDevices .devices');
-
-        bt.on(bt.bluetoothEvents.ScaningEnded, function () {
-
-            debugLog("ScaningEnded");
-
-            btAvailableDevices = bt.devices;
-            let btScanedDevicesDOM = document.querySelector('.settings .bluetooth .scanedDevices .devices');
-
-            //clear DOM element for new data after scanning
-            btScanedDevicesDOM.innerHTML = '';
-
-            for (let i = 0, l = btAvailableDevices.length; i < l; i++) {
-                btScanedDevicesDOM.innerHTML += '<div class="device" onclick="markDeviceToPair(this)"><div class="name">' + btAvailableDevices[i]['name'] + '</div><div class="mac">' + btAvailableDevices[i]['mac'] + '</div></div>';
-            }
-            debugLog("btAvailableDevices: ", btAvailableDevices);
-
-        });
-
-        bt.on(bt.bluetoothEvents.PassKey, function (passkey) {
-            debugLog('Confirm passkey:' + passkey);
-            bt.confirmPassKey(true);
-        });
-
-        bt.on(bt.bluetoothEvents.Connected, function () {
-            debugLog("Connected");
-
-            //clear DOM element for new data after scanning
-            btConnectedDevicesDOM.innerHTML = '';
-
-            btConnectedTo = bt.devices.filter(element => element.connected === 'yes');
-            btConnectedTo.forEach(element => {
-                btConnectedDevicesDOM.innerHTML += '<div class="device"><div class="name">' + element['name'] + '</div><div class="mac">' + element['mac'] + '</div></div>';
+            let btConnectedDevicesDOM = document.querySelector('.settings .bluetooth .pairedDevices .devices');
+    
+            bt.on(bt.bluetoothEvents.ScaningEnded, function () {
+    
+                debugLog("ScaningEnded");
+    
+                btAvailableDevices = bt.devices;
+                let btScanedDevicesDOM = document.querySelector('.settings .bluetooth .scanedDevices .devices');
+    
+                //clear DOM element for new data after scanning
+                btScanedDevicesDOM.innerHTML = '';
+    
+                for (let i = 0, l = btAvailableDevices.length; i < l; i++) {
+                    btScanedDevicesDOM.innerHTML += '<div class="device" onclick="markDeviceToPair(this)"><div class="name">' + btAvailableDevices[i]['name'] + '</div><div class="mac">' + btAvailableDevices[i]['mac'] + '</div></div>';
+                }
+                debugLog("btAvailableDevices: ", btAvailableDevices);
+    
             });
-        });
-
-
-        bt.on(bt.bluetoothEvents.Device, function () {
-
-            debugLog('Event Device');
-            //clear DOM element for new data after scanning
-            btConnectedDevicesDOM.innerHTML = '';
-
-            btConnectedTo = bt.devices.filter(element => element.connected === 'yes');
-            btConnectedTo.forEach(element => {
-                btConnectedDevicesDOM.innerHTML += '<div class="device"><div class="name">' + element['name'] + '</div><div class="mac">' + element['mac'] + '</div></div>';
+    
+            bt.on(bt.bluetoothEvents.PassKey, function (passkey) {
+                debugLog('Confirm passkey:' + passkey);
+                bt.confirmPassKey(true);
             });
-
-        });
+    
+            bt.on(bt.bluetoothEvents.Connected, function () {
+                debugLog("Connected");
+    
+                //clear DOM element for new data after scanning
+                btConnectedDevicesDOM.innerHTML = '';
+    
+                btConnectedTo = bt.devices.filter(element => element.connected === 'yes');
+                btConnectedTo.forEach(element => {
+                    btConnectedDevicesDOM.innerHTML += '<div class="device"><div class="name">' + element['name'] + '</div><div class="mac">' + element['mac'] + '</div></div>';
+                });
+            });
+    
+    
+            bt.on(bt.bluetoothEvents.Device, function () {
+    
+                debugLog('Event Device');
+                //clear DOM element for new data after scanning
+                btConnectedDevicesDOM.innerHTML = '';
+    
+                btConnectedTo = bt.devices.filter(element => element.connected === 'yes');
+                btConnectedTo.forEach(element => {
+                    btConnectedDevicesDOM.innerHTML += '<div class="device"><div class="name">' + element['name'] + '</div><div class="mac">' + element['mac'] + '</div></div>';
+                });
+    
+            });
+        } catch (error) {
+            console.log('Bluetooth error:', error);
+        }
+       
 
     });
 
